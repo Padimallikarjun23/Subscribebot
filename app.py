ae2afa4430c93474a2c0f63d7d808b89869e7a2f
Padimallikarjun23 <paditriveni1999@gmail.com>
2025-08-28 12:20:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '\Rockstar/'
-
+def home():
+    return "Bot is running!"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
